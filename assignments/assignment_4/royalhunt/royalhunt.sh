5cd2d331d9ae815f54b6b998190d4611f22c2b3a
Jonathan W. Vos <jonathan@platform45.com>
2020-08-30 21:37:56 +0200

    initialize

---
+++ assignments/assignment_4/royalhunt/royalhunt.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+minizinc royalhunt.mzn ./data/royalhunt_0.dzn
+minizinc royalhunt.mzn ./data/royalhunt_1.dzn
+minizinc royalhunt.mzn ./data/royalhunt_2.dzn
+minizinc royalhunt.mzn ./data/royalhunt_3.dzn
+minizinc royalhunt.mzn ./data/royalhunt_4.dzn
+# minizinc royalhunt.mzn ./data/royalhunt_5.dzn
+minizinc royalhunt.mzn ./data/royalhunt_6.dzn
+minizinc royalhunt.mzn ./data/royalhunt_7.dzn
+minizinc royalhunt.mzn ./data/royalhunt_16.dzn
+minizinc royalhunt.mzn ./data/royalhunt_17.dzn
